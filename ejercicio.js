3ae84d83556bd732a1e2970b12c3e92ef185598c
Alexander-Santacruz <161777501+Alexander-Santacruz@users.noreply.github.com>
2024-03-01 10:17:33 -0500

    Add files via upload

---
+++ ejercicio.js
@@ -0,0 +1,16 @@
+var nombre=("David");
+var apellido=("Santacruz")
+var edad="20";
+var num1="5"
+var num2="3"
+var suma=num1+num2;
+var tengonovia=true;
+console.log(nombre);
+console.log(apellido);
+console.log(edad)
+console.log(suma);
+console.log(tengonovia)
+
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(tengonovia));
